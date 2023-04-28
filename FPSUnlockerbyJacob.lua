1f733736bb22edc9a9247e68286eb285b9b372c2
GithuberWhateverJakub <70804792+GithuberWhateverJakub@users.noreply.github.com>
2023-04-28 16:27:17 +0200

    Update FPSUnlockerbyJacob.lua
    Main file, FPS unlocker

--- FPSUnlockerbyJacob.lua
+++ FPSUnlockerbyJacob.lua
@@ -88,7 +88,12 @@ wait(1)
 
 game:GetService("StarterGui"):SetCore("SendNotification",{
     Title = "Jakub's FPS Unlocker",
-    Text = "FPS UNLOCKED! Cap set to: "..unlocked_fps..""
+    Text = "FPS UNLOCKED! Cap set to: "..unlocked_fps.." have a nice day with me!"
 })
 
 setfpscap(unlocked_fps)
+
+game:GetService("StarterGui"):SetCore("SendNotification",{
+    Title = "Credits",
+    Text = "WhateverJakub (Creator) and nobody else 😎😎"
+})
