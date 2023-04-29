f2b7abb2d6c45cd7a83218538d83e9ade4455fd2
Jakub <70804792+WhateverJakub@users.noreply.github.com>
2023-04-29 15:23:12 +0200

    Update BetaTest.lua

--- BetaTest.lua
+++ BetaTest.lua
@@ -1,14 +1,5 @@
-
-
-game:GetService("StarterGui"):SetCore("SendNotification",{
-    Title = "Jakub's (not his) scripts",
-    Text = "Welcome to Jakub's scripts (incluides custom FPS unlocker!), hope you enjoy it! Loading script."
-})
-
-wait(4)
-
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/WhateverJakub/FPS-Unlocker/main/GUI.lua')))()
-local Window = OrionLib:MakeWindow({Name = "Jakub's Scripts", HidePremium = false, IntroText = "FPS Unlocker", SaveConfig = false, ConfigFolder = "FPSULL"})
+local Window = OrionLib:MakeWindow({Name = "FPS Unlocker or Locker", HidePremium = false, IntroText = "FPS Unlocker", SaveConfig = false, ConfigFolder = "FPSULL"})
 
 local FPSULLTAB = Window:MakeTab({
 	Name = "FPS Unlocker",
@@ -21,7 +12,6 @@ FPSULLTAB:AddButton({
     loadstring(game:HttpGet("https://raw.githubusercontent.com/WhateverJakub/FPS-Unlocker/main/FPSUnlockerPart"))() 
   	end    
 })
-
 FPSULLTAB:AddButton({
 	Name = "FPS Lock",
     loadstring(game:HttpGet("https://raw.githubusercontent.com/WhateverJakub/FPS-Unlocker/main/FPSUnlockerPart2"))() 
@@ -33,7 +23,6 @@ local AdminCMDS = Window:MakeTab({
 	Icon = "rbxassetid://1210153516",
 	PremiumOnly = false
 })
-
 AdminCMDS:AddButton({
 	Name = "Infinite Yield",
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
@@ -42,10 +31,33 @@ AdminCMDS:AddButton({
 
 local CB = Window:MakeTab({
 	Name = "CounterBlox",
-	Icon = "rbxassetid://13286976919",
+	Icon = "rbxassetid://9566409062",
 	PremiumOnly = false
 })
-
-
+CB:AddButton({
+	Name = "Cuteware",
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/WhateverJakub/FPS-Unlocker/main/CW.lua'),true))()
+  	end    
+})
+CB:AddButton({
+	Name = "Noovster",
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/NooVster/404/main/Source'),true))()
+  	end    
+})
+CB:AddButton({
+	Name = "pepsi.club",
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/deaddigi/pepsi.club/main/3.0/loader.lua", true))()
+  	end    
+})
+CB:AddButton({
+	Name = "ik3ac.hvh (pepsi.club but with more rage options)",
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/deaddigi/pepsi.club/main/3.0/loader.lua", true))()
+  	end    
+})
+CB:AddButton({
+	Name = "stormy.fixed",
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ik3ac/stormy-fixed-counter-blox-2023/main/.gitignore"))()
+  	end    
+})
 
 OrionLib:Init()
