f52a24d67d1217c8e9c56e9d01dbae0838570ca7
Jakub <70804792+WhateverJakub@users.noreply.github.com>
2023-04-29 15:26:22 +0200

    Update BetaTest.lua

--- BetaTest.lua
+++ BetaTest.lua
@@ -9,12 +9,13 @@ local FPSULLTAB = Window:MakeTab({
 
 FPSULLTAB:AddButton({
 	Name = "FPS Unlock",
+    Callback = function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/WhateverJakub/FPS-Unlocker/main/FPSUnlockerPart"))() 
   	end    
 })
-
 FPSULLTAB:AddButton({
 	Name = "FPS Lock",
+    Callback = function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/WhateverJakub/FPS-Unlocker/main/FPSUnlockerPart2"))() 
   	end    
 })
@@ -24,9 +25,9 @@ local AdminCMDS = Window:MakeTab({
 	Icon = "rbxassetid://1210153516",
 	PremiumOnly = false
 })
-
 AdminCMDS:AddButton({
 	Name = "Infinite Yield",
+    Callback = function()
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
   	end    
 })
@@ -36,33 +37,33 @@ local CB = Window:MakeTab({
 	Icon = "rbxassetid://9566409062",
 	PremiumOnly = false
 })
-
 CB:AddButton({
 	Name = "Cuteware",
+    Callback = function()
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/WhateverJakub/FPS-Unlocker/main/CW.lua'),true))()
   	end    
 })
-
 CB:AddButton({
 	Name = "Noovster",
+    Callback = function()
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/NooVster/404/main/Source'),true))()
   	end    
 })
-
 CB:AddButton({
 	Name = "pepsi.club",
+    Callback = function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/deaddigi/pepsi.club/main/3.0/loader.lua", true))()
   	end    
 })
-
 CB:AddButton({
 	Name = "ik3ac.hvh (pepsi.club but with more rage options)",
+    Callback = function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/deaddigi/pepsi.club/main/3.0/loader.lua", true))()
   	end    
 })
-
 CB:AddButton({
 	Name = "stormy.fixed",
+    Callback = function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ik3ac/stormy-fixed-counter-blox-2023/main/.gitignore"))()
   	end    
 })
