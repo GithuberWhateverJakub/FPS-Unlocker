cb4be61712d2671e143cda1c46a1c7586590a28d
Jakub <70804792+WhateverJakub@users.noreply.github.com>
2023-04-29 15:24:53 +0200

    Update BetaTest.lua

--- BetaTest.lua
+++ BetaTest.lua
@@ -12,6 +12,7 @@ FPSULLTAB:AddButton({
     loadstring(game:HttpGet("https://raw.githubusercontent.com/WhateverJakub/FPS-Unlocker/main/FPSUnlockerPart"))() 
   	end    
 })
+
 FPSULLTAB:AddButton({
 	Name = "FPS Lock",
     loadstring(game:HttpGet("https://raw.githubusercontent.com/WhateverJakub/FPS-Unlocker/main/FPSUnlockerPart2"))() 
@@ -23,6 +24,7 @@ local AdminCMDS = Window:MakeTab({
 	Icon = "rbxassetid://1210153516",
 	PremiumOnly = false
 })
+
 AdminCMDS:AddButton({
 	Name = "Infinite Yield",
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
@@ -34,26 +36,31 @@ local CB = Window:MakeTab({
 	Icon = "rbxassetid://9566409062",
 	PremiumOnly = false
 })
+
 CB:AddButton({
 	Name = "Cuteware",
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/WhateverJakub/FPS-Unlocker/main/CW.lua'),true))()
   	end    
 })
+
 CB:AddButton({
 	Name = "Noovster",
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/NooVster/404/main/Source'),true))()
   	end    
 })
+
 CB:AddButton({
 	Name = "pepsi.club",
     loadstring(game:HttpGet("https://raw.githubusercontent.com/deaddigi/pepsi.club/main/3.0/loader.lua", true))()
   	end    
 })
+
 CB:AddButton({
 	Name = "ik3ac.hvh (pepsi.club but with more rage options)",
     loadstring(game:HttpGet("https://raw.githubusercontent.com/deaddigi/pepsi.club/main/3.0/loader.lua", true))()
   	end    
 })
+
 CB:AddButton({
 	Name = "stormy.fixed",
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ik3ac/stormy-fixed-counter-blox-2023/main/.gitignore"))()
